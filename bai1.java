d26e0d1cf1d05f1bdf3881cf2b5f81388e1f4e2b
Breaveluv <nam.bule12@gmail.com>
2026-03-05 10:20:39 +0700

    nop PRJ

--- bai1.java
+++ bai1.java
@@ -6,65 +6,65 @@ public class bai1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> list = new ArrayList<>();
+
         System.out.print("Nhập số lượng phần tử: ");
         int n = sc.nextInt();
-        if(n<=0){
-            System.out.print(" Số lượng phần tử không hợp lệ ");
-        }else{
+
+        if (n <= 0) {
+            System.out.println("Số lượng phần tử không hợp lệ");
+        } else {
+            
             for (int i = 0; i < n; i++) {
                 list.add(sc.nextInt());
-
-
-
             }
-            int max = 0;
-            for (int i = 0; i < list.size(); i++) {
+
+            // 1. Tìm số lớn nhất 
+            int max = list.get(0);
+            for (int i = 1; i < list.size(); i++) {
                 if (list.get(i) > max) {
                     max = list.get(i);
                 }
             }
 
-            // Tính giá trị trung bình (double)
-            double Average = 0;
-            for (int i = 0; i < list.size(); i++) {
-                Average = Average + list.get(i);
-
-
-
-            }
-            Average = Average / list.size();
-            // Hiển thị danh sách theo thứ tự tăng dần
-            int []arr = new int[list.size()];
-            for (int i = 0; i< list.size(); i++){
-                arr[i]=list.get(i);
-
-            }
-
-
-
-
-
-            // Đếm số lượng chẵn lẻ
-            int Even = 0;
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i) % 2 == 0) {
-                    Even++;
-                }
-            }
-            int Odd = 0;
-            for (int i = 0; i < list.size(); i++) {
-                if(list.get(i) %2 != 0){
-                    Odd++;
+            // 2. Thuật toán sắp xếp tăng dần 
+         
+            for (int i = 0; i < list.size() - 1; i++) {
+                for (int j = 0; j < list.size() - 1 - i; j++) {
+                    if (list.get(j) > list.get(j + 1)) {
+                        int temp = list.get(j);
+                        list.set(j, list.get(j + 1));
+                        list.set(j + 1, temp);
+                    }
                 }
             }
 
-            System.out.println("Số lớn nhất trong danh sách:  "+ max );
-            System.out.println("Average: "+ Average );
-            System.out.println("Even: " + Even);
-            System.out.println("Odd: "+ Odd);
+            // 3. Tính giá trị trung bình (Kiểu double)
+            double sum = 0;
+            for (int num : list) {
+                sum += num;
+            }
+            double average = sum / list.size();
+
+            // 4. Đếm số lượng chẵn và lẻ
+            int even = 0;
+            for (int num : list) {
+                if (num % 2 == 0) {
+                    even++;
+                }
+            }
+            int odd = list.size() - even;
+
+            
+            System.out.println("Số lớn nhất trong danh sách: " + max);
+            
+            System.out.print("Danh sách theo thứ tự tăng dần: ");
+            for (int num : list) {
+                System.out.print(num + " ");
+            }
+            
+            System.out.println("\nAverage: " + average);
+            System.out.println("Even: " + even);
+            System.out.println("Odd: " + odd);
         }
-
-
-
     }
 }
--- bai2.java
+++ bai2.java
@@ -1,43 +1,65 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class bai2 {
-    public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-        System.out.print("Moi ban nhap chuỗi kí tự: ");
-        String s = sc.nextLine();
-        s.trim();
-        s = s.toLowerCase();
-        System.out.println(s);
-
-    if(s.equals("")){
-        System.out.println("Chuỗi không hợp lệ");
-    }else {
-        // Sử dụng Map + Thuật toán
-        // Sử dụng Map <Character, Integer> để lưu:
-        // Key: kí tự
-        // Value: Số lần xuất hiện
-        // Không phân biệt chữ viết hoa, viết thường
-        //Bỏ qua khoảng trắng
-        // Phải tự viết thuật toán: Tìm kí tự xuất hiện nhiều nhất, Sắp xếp các kí tự theo thứ tự apphabet(A->Z)
-       //
-
-        String a = s.toLowerCase();
-
-        System.out.println(a);
-        for(int i =0; i<s.length(); i++){
-            for(int j =0;j<a.length();j++){
-                System.out.print(a.charAt(j));
-            }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Mời bạn nhập chuỗi kí tự: ");
+        
+       
+        if (!sc.hasNextLine()) {
+            return;
         }
+        String input = sc.nextLine();
+        
+      
+        String processed = input.toLowerCase().replace(" ", "");
 
+        if (processed.equals("")) {
+            System.out.println("Lỗi: Chuỗi không hợp lệ");
+        } else {
+            Map<Character, Integer> map = new HashMap<>();
+            for (int i = 0; i < processed.length(); i++) {
+                char c = processed.charAt(i);
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
 
+           
+            List<Character> keys = new ArrayList<>(map.keySet());
+            for (int i = 0; i < keys.size() - 1; i++) {
+                for (int j = 0; j < keys.size() - 1 - i; j++) {
+                    if (keys.get(j) > keys.get(j + 1)) {
+                        char temp = keys.get(j);
+                        keys.set(j, keys.get(j + 1));
+                        keys.set(j + 1, temp);
+                    }
+                }
+            }
 
+           
+            int max = 0;
+            for (int freq : map.values()) {
+                if (freq > max) {
+                    max = freq;
+                }
+            }
+
+           
+            for (char c : keys) {
+                System.out.println(c + " xuất hiện: " + map.get(c) + " lần");
+            }
+
+            System.out.print("Ký tự xuất hiện nhiều nhất: ");
+            List<String> maxChars = new ArrayList<>();
+            for (char c : keys) {
+                if (map.get(c) == max) {
+                    maxChars.add(c + " (" + max + " lần)");
+                }
+            }
+            System.out.println(String.join(", ", maxChars));
+        }
     }
-
-
-    }
-
 }
